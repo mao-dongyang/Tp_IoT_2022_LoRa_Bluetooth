819ac2955f6fa40326be3ad2ed5fb7110fbd2c10
ZHANGJIEdjj <117640462+ZHANGJIEdjj@users.noreply.github.com>
2024-11-21 18:14:56 +0800

    Create MQTTSUB_LoRaSend.ino

---
+++ MQTTSUB_LoRaSend.ino
@@ -0,0 +1,93 @@
+#include <LoRa.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+// WiFi credentials
+const char* ssid = "Your_WiFi_SSID";
+const char* password = "Your_WiFi_Password";
+
+// MQTT broker
+const char* mqtt_server = "test.mosquitto.org";
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// LoRa parameters
+#define LORA_SCK 5
+#define LORA_MISO 19
+#define LORA_MOSI 27
+#define LORA_SS 18
+#define LORA_RST 14
+#define LORA_DIO0 26
+
+void setupWiFi() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+}
+
+void setupMQTT() {
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+    if (client.connect("LoRaReceiver")) {
+      Serial.println("Connected to MQTT");
+      client.subscribe("lora/parameters");
+    } else {
+      delay(1000);
+    }
+  }
+}
+
+void setupLoRa(float freq, int sf, int sb) {
+  LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO0);
+  if (!LoRa.begin(freq * 1E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  LoRa.setSpreadingFactor(sf);
+  LoRa.setSignalBandwidth(sb * 1E3);
+  Serial.println("LoRa Configured");
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String message = "";
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+  Serial.println("MQTT message received: " + message);
+
+  // Parse JSON and configure LoRa
+  DynamicJsonDocument doc(1024);
+  deserializeJson(doc, message);
+  int sb = doc["sb"];
+  int sf = doc["sf"];
+  float freq = doc["freq"];
+  setupLoRa(freq, sf, sb);
+}
+
+void setup() {
+  Serial.begin(115200);
+  setupWiFi();
+  setupMQTT();
+}
+
+void loop() {
+  if (!client.connected()) {
+    setupMQTT();
+  }
+  client.loop();
+
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    String received = "";
+    while (LoRa.available()) {
+      received += (char)LoRa.read();
+    }
+    Serial.println("LoRa received: " + received);
+  }
+}
